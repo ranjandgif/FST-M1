04fc02cfb98180353bec7268195d68d9f9826afd
ranjandgif <ranjani.dhandapani@ibm.com>
2024-06-09 19:00:13 +0530

    Add files via upload

---
+++ Java/Activities/activity1.java
@@ -0,0 +1,23 @@
+package Activity1;
+
+
+public class activity1 
+{
+public static void main(String[] args) 
+{
+    Car desier= new Car();
+
+    desier.color="black";
+    desier.make=2014;
+    desier.transmission="Manual";
+
+    desier.DisplayCharacterstics();
+    desier.accelarate();
+    desier.brake();
+
+
+
+
+}
+}
+
