b2cce4beb7fbaaeefe38b347de664d141841cf8c
ranjandgif <ranjani.dhandapani@ibm.com>
2024-06-13 12:14:00 +0530

    Add files via upload

---
+++ Python/Activities/activity1.py
@@ -0,0 +1,6 @@
+name = input("Enter username:")
+age=int( input("Enter age:"))
+year=(2024-age)+100
+statemnet="username is {0} and age i s{1}"
+print(statemnet.format(name,age))
+print(f"you will be truning 100 in {year}")
