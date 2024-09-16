15b6f77fc127293c225c0335bb4ea31962df3108
ranjandgif <ranjani.dhandapani@ibm.com>
2024-09-16 23:05:54 +0530

    Update Activity8.java

--- Appium/Activities/Activity8.java
+++ Appium/Activities/Activity8.java
@@ -1,29 +1,32 @@
-package activities;
-
-import io.appium.java_client.AppiumBy;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+package Activity;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 import java.util.List;
 
-public class Activity8 {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-    AndroidDriver driver;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+
+public class Activity8 {
     WebDriverWait wait;
+    AndroidDriver driver;
 
     @BeforeClass
-    public void SetUp() throws MalformedURLException {
+    public void beforeClass() throws MalformedURLException {
+        // Desired Capabilities
         UiAutomator2Options options = new UiAutomator2Options();
         options.setPlatformName("Android");
         options.setAutomationName("UiAutomator2");
@@ -31,45 +34,47 @@ public class Activity8 {
         options.setAppActivity("com.google.android.apps.chrome.Main");
         options.noReset();
 
+        // Server URL
         URL serverURL = new URL("http://localhost:4723/wd/hub");
 
+        // Driver initialization
         driver = new AndroidDriver(serverURL, options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-
-        driver.get("https://v1.training-support.net/selenium");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        
+        // Open Selenium page
+        driver.get("https://www.training-support.net/selenium");
     }
 
     @Test
-    public void todoListTest() {
-        Dimension dims = driver.manage().window().getSize();
-        Point start = new Point((int)(dims.getWidth() * 0.5), (int)(dims.getHeight() * 0.8));
-        Point end = new Point((int)(dims.getWidth() * 0.5), (int)(dims.getHeight() * 0.6));
+    public void scrollIntoViewTest() {
+        String UiScrollable = "UiScrollable(UiSelector().scrollable(true))";
+        // Wait for the page to load
+        wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.view.View")));
 
-        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.Button[@text=\"Get Started!\"]"))).click();
+        // Just scroll
+        driver.findElement(AppiumBy.androidUIAutomator(UiScrollable + ".flingForward()"));
+        // Locate element
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Tab opener']")).click();
 
-        ActionsBase.doSwipe(driver, start, end, 50);
+        // Scroll and locate elements
+        //driver.findElement(AppiumBy.androidUIAutomator(
+                //UiScrollable + ".scrollTextIntoView(\"Tab Opener\")"
+        //)).click();
 
-        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.TextView[@text=\"To-Do List\"]"))).click();
-        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.TextView[@text=\"To-Do List\"]")));
+        // Wait for the page to load
+        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Click Me!")));
+        // Print page title
+        String pageTitle = driver.findElement(AppiumBy.xpath("//android.widget.TextView[contains(@text, 'New Tab')]")).getText();
+        System.out.println("Page title is: " + pageTitle);
 
-        int sizeBeforeTask = driver.findElements(AppiumBy.xpath("//android.widget.TextView")).size();
-        driver.findElement(AppiumBy.xpath("//android.webkit.WebView[@text=\"Todo List\"]/android.view.View/android.view.View/android.view.View[1]/android.widget.EditText")).sendKeys("Add tasks to list");
-        driver.findElement(AppiumBy.xpath("//android.widget.Button[@text=\"Add Task\"]")).click();
-        driver.findElement(AppiumBy.xpath("//android.webkit.WebView[@text=\"Todo List\"]/android.view.View/android.view.View/android.view.View[1]/android.widget.EditText")).sendKeys("Get number of tasks");
-        driver.findElement(AppiumBy.xpath("//android.widget.Button[@text=\"Add Task\"]")).click();
-        driver.findElement(AppiumBy.xpath("//android.webkit.WebView[@text=\"Todo List\"]/android.view.View/android.view.View/android.view.View[1]/android.widget.EditText")).sendKeys("Clear the list");
-        driver.findElement(AppiumBy.xpath("//android.widget.Button[@text=\"Add Task\"]")).click();
+        // Assertion
+        Assert.assertEquals(pageTitle, "New Tab Opener");
+    }
 
-        //Number of task
-        List<WebElement> tasks = driver.findElements(AppiumBy.xpath("//android.widget.TextView"));
-        Assert.assertEquals(tasks.size(), sizeBeforeTask+3);
-
-        //Strike
-        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Add tasks to list\"]")).click();
-        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Get number of tasks\"]")).click();
-        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Clear the list\"]")).click();
-
-        //Clear
-        driver.findElement(AppiumBy.xpath("//android.webkit.WebView[@text=\"Todo List\"]/android.view.View/android.view.View/android.view.View[3]")).click();
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
     }
 }
+
+
